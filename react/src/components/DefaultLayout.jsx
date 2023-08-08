546b40d089d4aec7ad20d24332a1ac9ed81aad01
mohamad chamseddine <mohamadchams123@gmail.com>
2023-08-08 23:00:09 +0300

    show user's full name

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,14 +1,23 @@
 import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import { useEffect } from "react";
+import axiosClient from "../axios-client";
 
 export default function DefaultLayout() {
-    const {user,token} = useStateContext()
+    const {user,token,setUser} = useStateContext()
     if(!token){
         return <Navigate to={'/login'}/>
     }
     const onLogout = (ev) => {
         ev.preventDefault()
     }
+    useEffect(() => {
+        axiosClient.get('/user')
+            .then(({data}) => {
+                setUser(data)
+            })
+    }, [])
+
     return (
         <div id="defaultLayout">
             <aside>
