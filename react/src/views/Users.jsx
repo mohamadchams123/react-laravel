ee59222495efba125d34f7f3df1a19233a0f959d
mohamad chamseddine <mohamadchams123@gmail.com>
2023-08-09 00:53:09 +0300

    users CRUD backend (in api)

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react"
+import axiosClient from "../axios-client";
+
 export default function Users() {
+    const [users, setUser] = useState([]);
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+        getUsers();
+    }, [])
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+            .then(({data}) => {
+                setLoading(false)
+                console.log(data);
+            })
+            .catch(() => {
+                setLoading(false)
+            })
+    }
     return (
         <div>
             Users
